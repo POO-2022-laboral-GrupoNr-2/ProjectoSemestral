401746ed06af285223a993be5c2be6b6694ea00b
EdilsonRicardo <ricardoedilsondossantos@gmail.com>
2022-11-02 21:27:19 +0200

    Actualizacao da Tela Relatorio de Estadia

--- SistemaHoteleiro/src/View/TelaRelatorio.java
+++ SistemaHoteleiro/src/View/TelaRelatorio.java
@@ -35,20 +35,20 @@ public class TelaRelatorio extends javax.swing.JFrame {
 
     public void showBarChart(){
          DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.setValue(8, "A", "Jan");
-        dataset.setValue(3, "A", "Fev");
-        dataset.setValue(4, "A", "Mar");
-        dataset.setValue(5, "A", "Abr");
-        dataset.setValue(20, "A", "Mai");
-        dataset.setValue(2, "A", "Jun");
-        dataset.setValue(2, "A", "Jul");
-        dataset.setValue(9, "A", "Ago");
-        dataset.setValue(3, "A", "Set");
-        dataset.setValue(4, "A", "Out");
-        dataset.setValue(5, "A", "Nov");
-        dataset.setValue(6, "A", "Dez");
+        dataset.setValue(8, "Ab", "Jan");
+        dataset.setValue(3, "Ab", "Fev");
+        dataset.setValue(4, "Ab", "Mar");
+        dataset.setValue(5, "Ab", "Abr");
+        dataset.setValue(20, "Ab", "Mai");
+        dataset.setValue(2, "Ab", "Jun");
+        dataset.setValue(2, "Ab", "Jul");
+        dataset.setValue(9, "Ab", "Ago");
+        dataset.setValue(3, "Ab", "Set");
+        dataset.setValue(4, "Ab", "Out");
+        dataset.setValue(5, "Ab", "Nov");
+        dataset.setValue(6, "Ab", "Dez");
         
-        JFreeChart chart = ChartFactory.createBarChart(" ","","", 
+        JFreeChart chart = ChartFactory.createBarChart(" ","Meses","Quantidades", 
                 dataset, PlotOrientation.VERTICAL, false,true,false);
         
         CategoryPlot categoryPlot = chart.getCategoryPlot();
@@ -78,44 +78,239 @@ public class TelaRelatorio extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         panelGraficoBarras = new javax.swing.JPanel();
+        lblTituloNoTopo = new javax.swing.JLabel();
+        lblJan = new javax.swing.JLabel();
+        lblFev = new javax.swing.JLabel();
+        lblMar = new javax.swing.JLabel();
+        lblAbr = new javax.swing.JLabel();
+        lblMai = new javax.swing.JLabel();
+        lblJun = new javax.swing.JLabel();
+        lblAgo = new javax.swing.JLabel();
+        lblSet = new javax.swing.JLabel();
+        lblOut = new javax.swing.JLabel();
+        lblNov = new javax.swing.JLabel();
+        lblDez = new javax.swing.JLabel();
+        lblJul = new javax.swing.JLabel();
+        txtJan = new javax.swing.JTextField();
+        txtFev = new javax.swing.JTextField();
+        txtMar = new javax.swing.JTextField();
+        txtAbr = new javax.swing.JTextField();
+        txtMai = new javax.swing.JTextField();
+        txtJun = new javax.swing.JTextField();
+        txtJul = new javax.swing.JTextField();
+        txtAgo = new javax.swing.JTextField();
+        txtSet = new javax.swing.JTextField();
+        txtOut = new javax.swing.JTextField();
+        txtNov = new javax.swing.JTextField();
+        txtDez = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Relatorio de Estadias");
         setResizable(false);
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(0, 81, 114));
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBackground(new java.awt.Color(0, 81, 114));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         panelGraficoBarras.setLayout(new java.awt.BorderLayout());
-        jPanel2.add(panelGraficoBarras, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 450, 240));
+        jPanel2.add(panelGraficoBarras, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 670, 240));
+
+        lblTituloNoTopo.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        lblTituloNoTopo.setForeground(new java.awt.Color(255, 255, 255));
+        lblTituloNoTopo.setText("RELATÓRIO DE ESTADIAS MENSAIS");
+        jPanel2.add(lblTituloNoTopo, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 10, -1, -1));
+
+        lblJan.setForeground(new java.awt.Color(255, 255, 255));
+        lblJan.setText("Janeiro");
+
+        lblFev.setForeground(new java.awt.Color(255, 255, 255));
+        lblFev.setText("Fevereiro");
+
+        lblMar.setForeground(new java.awt.Color(255, 255, 255));
+        lblMar.setText("Março");
+
+        lblAbr.setForeground(new java.awt.Color(255, 255, 255));
+        lblAbr.setText("Abril");
+
+        lblMai.setForeground(new java.awt.Color(255, 255, 255));
+        lblMai.setText("Maio");
+
+        lblJun.setForeground(new java.awt.Color(255, 255, 255));
+        lblJun.setText("Junho");
+
+        lblAgo.setForeground(new java.awt.Color(255, 255, 255));
+        lblAgo.setText("Agosto");
+
+        lblSet.setForeground(new java.awt.Color(255, 255, 255));
+        lblSet.setText("Setembro");
+
+        lblOut.setForeground(new java.awt.Color(255, 255, 255));
+        lblOut.setText("Outubro");
+
+        lblNov.setForeground(new java.awt.Color(255, 255, 255));
+        lblNov.setText("Novembro");
+
+        lblDez.setForeground(new java.awt.Color(255, 255, 255));
+        lblDez.setText("Dezembro");
+
+        lblJul.setForeground(new java.awt.Color(255, 255, 255));
+        lblJul.setText("Julho");
+
+        txtJan.setEditable(false);
+        txtJan.setFocusable(false);
+
+        txtFev.setEditable(false);
+        txtFev.setFocusable(false);
+
+        txtMar.setEditable(false);
+        txtMar.setFocusable(false);
+
+        txtAbr.setEditable(false);
+        txtAbr.setFocusable(false);
+
+        txtMai.setEditable(false);
+        txtMai.setFocusable(false);
+
+        txtJun.setEditable(false);
+        txtJun.setFocusable(false);
+
+        txtJul.setEditable(false);
+        txtJul.setFocusable(false);
+
+        txtAgo.setEditable(false);
+        txtAgo.setFocusable(false);
+
+        txtSet.setEditable(false);
+        txtSet.setFocusable(false);
+
+        txtOut.setEditable(false);
+        txtOut.setFocusable(false);
+
+        txtNov.setEditable(false);
+        txtNov.setFocusable(false);
+
+        txtDez.setEditable(false);
+        txtDez.setFocusable(false);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(946, 946, 946))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(192, 192, 192)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lblMar)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(lblFev)
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                    .addGap(11, 11, 11)
+                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(lblJan, javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                                .addComponent(lblJun)
+                                                .addComponent(lblMai))
+                                            .addGap(2, 2, 2)))))
+                            .addComponent(lblAbr))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtMai, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)
+                                    .addComponent(txtJun, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE))
+                                .addGap(104, 104, 104)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblNov, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(lblDez, javax.swing.GroupLayout.Alignment.TRAILING)))
+                            .addComponent(lblSet)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(txtFev, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtAbr, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(77, 77, 77)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(46, 46, 46)
+                                        .addComponent(lblAgo))
+                                    .addComponent(lblOut, javax.swing.GroupLayout.Alignment.TRAILING)))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(txtJan, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(lblJul))
+                            .addComponent(txtMar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(txtAgo, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)
+                            .addComponent(txtSet, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)
+                            .addComponent(txtOut, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)
+                            .addComponent(txtNov, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE)
+                            .addComponent(txtDez, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtJul, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(44, 44, 44)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(53, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(30, 30, 30)
+                .addContainerGap(68, Short.MAX_VALUE)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(174, Short.MAX_VALUE))
+                .addGap(27, 27, 27)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblJan)
+                    .addComponent(txtJan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblJul)
+                    .addComponent(txtJul, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblFev)
+                    .addComponent(txtFev, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblAgo)
+                    .addComponent(txtAgo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblMar)
+                    .addComponent(txtMar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblSet)
+                    .addComponent(txtSet, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(15, 15, 15)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblAbr)
+                    .addComponent(txtAbr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblOut)
+                    .addComponent(txtOut, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblMai)
+                    .addComponent(txtMai, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblNov)
+                    .addComponent(txtNov, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblJun)
+                    .addComponent(txtJun, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblDez)
+                    .addComponent(txtDez, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 463, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -160,6 +355,31 @@ public class TelaRelatorio extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lblAbr;
+    private javax.swing.JLabel lblAgo;
+    private javax.swing.JLabel lblDez;
+    private javax.swing.JLabel lblFev;
+    private javax.swing.JLabel lblJan;
+    private javax.swing.JLabel lblJul;
+    private javax.swing.JLabel lblJun;
+    private javax.swing.JLabel lblMai;
+    private javax.swing.JLabel lblMar;
+    private javax.swing.JLabel lblNov;
+    private javax.swing.JLabel lblOut;
+    private javax.swing.JLabel lblSet;
+    private javax.swing.JLabel lblTituloNoTopo;
     private javax.swing.JPanel panelGraficoBarras;
+    private javax.swing.JTextField txtAbr;
+    private javax.swing.JTextField txtAgo;
+    private javax.swing.JTextField txtDez;
+    private javax.swing.JTextField txtFev;
+    private javax.swing.JTextField txtJan;
+    private javax.swing.JTextField txtJul;
+    private javax.swing.JTextField txtJun;
+    private javax.swing.JTextField txtMai;
+    private javax.swing.JTextField txtMar;
+    private javax.swing.JTextField txtNov;
+    private javax.swing.JTextField txtOut;
+    private javax.swing.JTextField txtSet;
     // End of variables declaration//GEN-END:variables
 }
